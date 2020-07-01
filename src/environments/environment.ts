2c969d7a2b0faf8b2a698bd0604f2ed0bcbbf8c6
Ricardo Arrobo <ricardoarrobo@gmail.com>
2020-06-30 23:18:18 -0500

    refactor: changed apiUrl env var

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:1337'
+  apiUrl: 'https://mighty-oasis-26673.herokuapp.com'
+  // apiUrl: 'http://localhost:1337'
 };
 
 /*
