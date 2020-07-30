769fc5a3ceb2f1f3ee92697586eeacabce12dc76
Ricardo Arrobo <ricardoarrobo@gmail.com>
2020-07-29 21:36:08 -0500

    fix: style on header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-header',
-    templateUrl: './header.component.html'
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
     currentUser = JSON.parse(localStorage.getItem('currentUser'));
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  // apiUrl: 'https://mighty-oasis-26673.herokuapp.com'
-  apiUrl: 'http://localhost:1337'
+  production: true,
+  apiUrl: 'https://mighty-oasis-26673.herokuapp.com'
+  // apiUrl: 'http://localhost:1337'
 };
 
 /*
